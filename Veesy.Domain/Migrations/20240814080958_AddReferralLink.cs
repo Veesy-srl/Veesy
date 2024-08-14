99d1d140aa2b0486d2e2515f9e6392e5613d63a3
lorevetto11 <lorenzo.vettori3@stud.unifi.it>
2024-08-14 15:23:42 +0200

    Missing migration files;

---
+++ Veesy.Domain/Migrations/20240814080958_AddReferralLink.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Veesy.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferralLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReferralLinks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Endpoint = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RedirectUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Enable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReferralLinks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReferralLinkTrackings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReferralLinkId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Ip = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Referer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastAccess = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Latitude = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Longitude = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Browser = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeviceType = table.Column<int>(type: "int", nullable: false),
+                    UserAgent = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReferralLinkTrackings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReferralLinkTrackings_ReferralLinks_ReferralLinkId",
+                        column: x => x.ReferralLinkId,
+                        principalTable: "ReferralLinks",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReferralLinkTrackings_ReferralLinkId",
+                table: "ReferralLinkTrackings",
+                column: "ReferralLinkId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReferralLinkTrackings");
+
+            migrationBuilder.DropTable(
+                name: "ReferralLinks");
+        }
+    }
+}
